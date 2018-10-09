a00aae2ae12aee5ac00b82a5180ff633a2a3d059
Milos <protic023@gmail.com>
2018-10-09 15:17:00 +0200

    demo app

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,29 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var header = require('gulp-header');
-var pkg = require('./package.json');
 
 var source = [
     'src/core/plazar-js-core.js',
     'src/statics/plazar-js-http.js',
     'src/statics/plazar-js-binder.js',
-    'src/statics/plazar-js-array.js',
-    'src/statics/plazar-js-string.js',
-    'src/statics/plazar-js-dom.js',
-    'src/statics/plazar-js-object.js',
+    'src/statics/utils/plazar-js-array.js',
+    'src/statics/utils/plazar-js-string.js',
+    'src/statics/utils/plazar-js-dom.js',
+    'src/statics/utils/plazar-js-object.js',
     'src/components/base/plazar-js-base.js',
-    'src/components/base/plazar-js-class.js',
-    'src/components/base/plazar-js-component.js',
-    'src/components/base/plazar-js-mixin.js'
+    'src/components/plazar-js-class.js',
+    'src/components/plazar-js-component.js',
+    'src/components/plazar-js-mixin.js'
 ];
 
 var banner = function() {
-    return [
-      '// version: ' + pkg.version,
-      '// author: ' + pkg.author,
-      '// license: ' + pkg.licenses[0].type
-    ].join('\n') + '\n';
+    return '// Plazar JS';
 };
 
 gulp.task('build', function(){
-    return gulp.source(source)
-        .pipe(concat('plazar-js.js'))
+    return gulp.src(source)
+        .pipe(concat('plazar-js.min.js'))
+        .pipe(uglify())
         .pipe(header(banner()))
         .pipe(gulp.dest('dist'));
 });
