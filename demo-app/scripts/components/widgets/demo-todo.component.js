cf0add0a04813332da12032aa442ec3af1a840e1
Milos <protic023@gmail.com>
2018-10-12 09:47:26 +0200

    removed debugger

--- demo-app/scripts/components/widgets/demo-todo.component.js
+++ demo-app/scripts/components/widgets/demo-todo.component.js
@@ -59,7 +59,6 @@ plz.define('todo-component', function() {
                 fn: 'addTodo'
             });
             plz.arr.clear(this.viewModel.todos); // TODO: See why viewModel still has todos after destroy
-            debugger;
             var todos = this.todoService.get();
             plz.forEach(todos, function(todo) {
                 this.viewModel.todos.push(plz.obj.clone(todo));
