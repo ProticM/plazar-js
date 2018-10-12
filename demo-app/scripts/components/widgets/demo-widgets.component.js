2a0285109ad3b8515bfae904b0b2d22d0d7e6152
Milos <protic023@gmail.com>
2018-10-12 11:53:16 +0200

    minor change

--- demo-app/scripts/components/widgets/demo-widgets.component.js
+++ demo-app/scripts/components/widgets/demo-widgets.component.js
@@ -2,7 +2,7 @@ plz.define('widgets-component', {
     ownerType: 'base-component',
     mixins: ['page-mixin'],
     template: '<div class="panel"><p class="panel-heading">' + 
-        'Example widgets are built in purpose of this demo. Hopefully this project will grow and we will build set of controls to be supported out of the box.</p>' +
+        'Example widgets are built in purpose of this demo. Hopefully PlazarJS will grow and we will build set of controls to be supported out of the box.</p>' +
         '<div class="columns is-marginless">' +
         '<div class="column todo"></div>' +
         '<div class="column grid"></div>' +
