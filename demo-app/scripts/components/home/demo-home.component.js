fe00158c3ae2a8a2663e0656bffbb43400e13d7c
Milos <protic023@gmail.com>
2018-10-15 08:20:38 +0200

    working on demo app

--- demo-app/scripts/components/home/demo-home.component.js
+++ demo-app/scripts/components/home/demo-home.component.js
@@ -3,7 +3,7 @@ plz.define('home-component', {
     mixins: ['page-mixin'],
     template: '<div>Welcome to PlazarJS Demo Application. This demo uses <a class="has-text-lightblue" href="https://bulma.io/" target="_blank">Bulma CSS Framework</a> ' + 
         'for styling therefore make sure that your browser supports ' +
-        '<a class="has-text-lightblue" href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox" target="_blank">Flexbox</a>. Note that usage of this framework is not required and it\'\s used to simplify this demo while focusing on PlazarJS features.' + 
+        '<a class="has-text-lightblue" href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox" target="_blank">Flexbox</a>. Note that usage of this framework is not required and it\'\s used to simplify this demo while focusing on PlazarJS features. ' + 
         'We built this demo by using inline and pre rendered templates, but every template can be loaded asynchronously from the server while initializing component.' +
         '</div>',
     renderTo: 'section.app-body'
