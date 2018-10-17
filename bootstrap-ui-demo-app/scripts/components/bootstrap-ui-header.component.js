25ac1e014224efee11bfb81a445776ff401b69c5
Milos <protic023@gmail.com>
2018-10-17 09:19:39 +0200

    working on bootstrap demo app

--- bootstrap-ui-demo-app/scripts/components/bootstrap-ui-header.component.js
+++ bootstrap-ui-demo-app/scripts/components/bootstrap-ui-header.component.js
@@ -5,18 +5,36 @@ plz.define('bootstrap-ui-header-component', {
     position: 'top',
     autoLoad: true,
     theme: 'dark',
+    brand: {
+        type: 'text',
+        value: 'Company Name'
+    },
     menu: {
         items: [{
-            text: 'Helloo'
+            text: 'Menu Item 1'
+        },{
+            text: 'Menu Item 2'
         }, {
             type: 'ui-bootstrap-dropdown',
-            split: true,
-            text: 'Menu Item 1',
+            inNav: true,
+            text: 'Menu Item 3',
             menuItems: [{
-                text: 'Menu Item 1.1'
+                text: 'Menu Item 3.1'
             },{
-                text: 'Menu Item 1.2'
+                text: 'Menu Item 3.2'
             }]
         }]
-    }
+    },
+    components: [{
+        type: 'ui-bootstrap-form',
+        css: ['form-inline'],
+        renderTo: 'div.navbar-collapse',
+        button: {
+            appearance: 'success'
+        },
+        components: [{
+            type: 'ui-bootstrap-input',
+            css: ['mr-1']
+        }]
+    }]
 });
--- bootstrap-ui-demo-app/scripts/components/bootstrap-ui-layout.component.js
+++ bootstrap-ui-demo-app/scripts/components/bootstrap-ui-layout.component.js
@@ -3,29 +3,64 @@ plz.define('bootstrap-ui-layout-component', {
     templateSelector: 'app-layout',
     autoLoad: true,
     components: [{
-        type: 'ui-bootstrap-card',
+        type: 'ui-bootstrap-container',
         renderTo: 'root',
-        footer: false,
-        header: false,
-        body: {
-            title: 'Hello',
-            text: 'Welcome'
-        },
-        buttons: [{
-            text: 'Button 1',
-            appearance: 'success'
-        }],
-        components: [{
-            type: 'ui-bootstrap-dropdown',
-            text: 'States',
-            css: ['pull-right'],
-            split: true,
-            dropPosition: 'left',
-            renderTo: 'div.card-footer',
-            appearance: 'danger',
-            menuItems: [{
-                text: 'Serbia',
-                href: 'http://www.google.com'
+        renderAs: 'jumbotron',
+        fluid: true,
+        jumbotron: {
+            title: {
+                text:'Welcome to Bootstrap Demo',
+                size: 3
+            },
+            leadText: 'Easily Create Application Layouts Using PlazarJS Bootstrap-UI',
+            buttons: [{
+                text: 'Getting Started',
+                size: 'lg',
+                onClick: function() {
+                    alert('Getting Started');
+                }
+            }]
+        }
+    }, {
+        type: 'ui-bootstrap-container',
+        renderTo: 'root',
+        components:[{
+            type: 'ui-bootstrap-container',
+            renderAs: 'row',
+            components:[{
+                type: 'ui-bootstrap-container',
+                renderAs: 'column',
+                column: {
+                    lg: 4,
+                    md: 2,
+                    sm: 3
+                },
+                components: [{
+                    type: 'ui-bootstrap-card',
+                    header: {
+                        text: 'Hello Card 1'
+                    },
+                    footer: false,
+                    body: {
+                        text: 'Lorem ipsum'
+                    }
+                }]
+            }, {
+                type: 'ui-bootstrap-container',
+                renderAs: 'column',
+                column: {
+                    lg: 4,
+                    md: 4,
+                    sm: 1
+                }
+            }, {
+                type: 'ui-bootstrap-container',
+                renderAs: 'column',
+                column: {
+                    lg: 4,
+                    md: 4,
+                    sm: 1
+                }
             }]
         }]
     }]
