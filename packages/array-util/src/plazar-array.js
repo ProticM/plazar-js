e8b13948a87f6c05d1d452545f3f6b3b67fdd189
Milos <protic023@gmail.com>
2018-11-18 20:31:48 +0100

    removed wrapper functions

--- packages/array-util/src/plazar-array.js
+++ packages/array-util/src/plazar-array.js
@@ -1,58 +1,53 @@
-﻿(function(pz) {
-    'use strict';
+﻿return {
+    clear: function (array) {
+        if (pz.isEmpty(array)) {
+            return;
+        };
 
-    return {
-        clear: function (array) {
-            if (pz.isEmpty(array)) {
-                return;
-            };
+        array.splice(0, array.length);
+    },
 
-            array.splice(0, array.length);
-        },
+    find: function (callback, arr, scope) {
+        return pz.find(callback, arr, scope);
+    },
 
-        find: function (callback, arr, scope) {
-            return pz.find(callback, arr, scope);
-        },
+    contains: function (array, item, fromIndex) {
+        var isFunction = pz.isFunction(item);
 
-        contains: function (array, item, fromIndex) {
-            var isFunction = pz.isFunction(item);
+        return isFunction ? (function () {
+            var el = pz.arr.find(item, array);
+            return !pz.isEmpty(el);
+        })() : pz.isEmpty(array) ? false : array.indexOf(item, fromIndex) != -1;
+    },
 
-            return isFunction ? (function () {
-                var el = pz.arr.find(item, array);
-                return !pz.isEmpty(el);
-            })() : pz.isEmpty(array) ? false : array.indexOf(item, fromIndex) != -1;
-        },
+    filter: function (callback, array) {
+        var res = array.filter(callback);
+        callback = null;
+        return res;
+    },
 
-        filter: function (callback, array) {
-            var res = array.filter(callback);
-            callback = null;
-            return res;
-        },
+    merge: function () {
+        var args = Array.prototype.slice.call(arguments),
+            resultArray = [];
 
-        merge: function () {
-            var args = Array.prototype.slice.call(arguments),
-                resultArray = [];
+        pz.forEach(args, function (array) {
+            resultArray = resultArray.concat(array);
+        });
 
-            pz.forEach(args, function (array) {
-                resultArray = resultArray.concat(array);
-            });
+        return resultArray;
+    },
 
-            return resultArray;
-        },
+    map: function (callback, array, scope) {
+        var result = array.map(callback, scope);
+        callback = null;
+        return result;
+    },
 
-        map: function (callback, array, scope) {
-            var result = array.map(callback, scope);
-            callback = null;
-            return result;
-        },
+    removeAt: function (array, index) {
+        if (pz.isEmpty(array) || pz.isEmpty(index)) {
+            return;
+        };
 
-        removeAt: function (array, index) {
-            if (pz.isEmpty(array) || pz.isEmpty(index)) {
-                return;
-            };
-
-            array.splice(index, 1);
-        }
-    };
-
-})();
+        array.splice(index, 1);
+    }
+};
--- packages/http/src/plazar-http.js
+++ packages/http/src/plazar-http.js
@@ -1,148 +1,144 @@
-﻿(function (pz) {
-    'use strict';
+﻿var _requests, _const = {
+    optionsRequred: 'Can not instantiate http request without options defined',
+    minConfigNotProfided: 'Minimal configuration for ajax call was not provided. Please check you setup for following options [url], [method]',
+    requestStates: {
+        done: 4
+    },
+    requestStatus: {
+        abort: 0
+    }
+}, _types = {
+    post: 'POST',
+    get: 'GET',
+    put: 'PUT',
+    data: {
+        json: 'json',
+        html: 'html'
+    }
+}, _createXHR = function () {
+    // TODO: Add support for active x object?
+    // ActiveXObject('MSXML2.XMLHTTP.3.0');
+    // ActiveXObject('MSXML2.XMLHTTP');
+    // ActiveXObject('Microsoft.XMLHTTP');
+    return new XMLHttpRequest();
+}, _checkMinimalConfiguration = function (options) {
+    // add more if needed
 
-    var _requests, _const = {
-        optionsRequred: 'Can not instantiate http request without options defined',
-        minConfigNotProfided: 'Minimal configuration for ajax call was not provided. Please check you setup for following options [url], [method]',
-        requestStates: {
-            done: 4
-        },
-        requestStatus: {
-            abort: 0
-        }
-    }, _types = {
-        post: 'POST',
-        get: 'GET',
-        put: 'PUT',
-        data: {
-            json: 'json',
-            html: 'html'
-        }
-    }, _createXHR = function () {
-        // TODO: Add support for active x object?
-        // ActiveXObject('MSXML2.XMLHTTP.3.0');
-        // ActiveXObject('MSXML2.XMLHTTP');
-        // ActiveXObject('Microsoft.XMLHTTP');
-        return new XMLHttpRequest();
-    }, _checkMinimalConfiguration = function (options) {
-        // add more if needed
+    var isOK = !pz.isEmpty(options.url) &&
+        !pz.isEmpty(options.method);
 
-        var isOK = !pz.isEmpty(options.url) &&
-            !pz.isEmpty(options.method);
+    if (!isOK) {
+        throw new Error(_const.minConfigNotProfided);
+    };
+}, _configureAndInvokeXHR = function (request, options) {
+    var xhr = request.xhr, callback = options.success,
+        eCallback = options.fail, aCallback = options.abort,
+        dataType = options.dataType || _types.data.json;
 
-        if (!isOK) {
-            throw new Error(_const.minConfigNotProfided);
-        };
-    }, _configureAndInvokeXHR = function (request, options) {
-        var xhr = request.xhr, callback = options.success,
-            eCallback = options.fail, aCallback = options.abort,
-            dataType = options.dataType || _types.data.json;
+    xhr.onreadystatechange = function () {
 
-        xhr.onreadystatechange = function () {
-
-            if (this.readyState == _const.requestStates.done
-                && this.status == _const.requestStatus.abort) {
-                return;
-            };
-
-            if (this.readyState == _const.requestStates.done && !pz.isEmpty(callback)) {
-                var result = {
-                    request: this,
-                    data: (dataType == _types.data.json ? pz.toJSON(this.responseText) : this.responseText),
-                    dataType: dataType
-                };
-                callback.call(this, result);
-                delete _requests[request.id];
-            };
+        if (this.readyState == _const.requestStates.done
+            && this.status == _const.requestStatus.abort) {
+            return;
         };
 
-        xhr.onerror = function (e) {
+        if (this.readyState == _const.requestStates.done && !pz.isEmpty(callback)) {
+            var result = {
+                request: this,
+                data: (dataType == _types.data.json ? pz.toJSON(this.responseText) : this.responseText),
+                dataType: dataType
+            };
+            callback.call(this, result);
             delete _requests[request.id];
-
-            if (eCallback) {
-                eCallback(e.target);
-            } else {
-                throw new Error(e.target.statusText);
-            };
         };
-
-        xhr.onabort = function (e) {
-            if (aCallback) {
-                aCallback(e);
-            };
-        };
-
-        xhr.open(options.method, options.url, true);
-
-        if (pz.isString(options.data)) {
-            options.data = pz.toJSON(options.data);
-        };
-
-        xhr.send(options.data || null);
     };
 
-    return {
-        requests: {},
-        latestRequestId: null,
-        request: function (options) {
+    xhr.onerror = function (e) {
+        delete _requests[request.id];
 
-            if (pz.isEmpty(options)) {
-                throw new Error(_const.optionsRequred);
-            };
+        if (eCallback) {
+            eCallback(e.target);
+        } else {
+            throw new Error(e.target.statusText);
+        };
+    };
 
-            _checkMinimalConfiguration(options);
+    xhr.onabort = function (e) {
+        if (aCallback) {
+            aCallback(e);
+        };
+    };
 
-            var request = {
-                id: pz.guid(),
-                aborted: false,
-                options: options,
-                xhr: _createXHR(),
-                abort: function myfunction() {
-                    this.xhr.abort();
-                    this.xhr = null;
-                    this.aborted = true;
-                }
-            };
+    xhr.open(options.method, options.url, true);
 
-            this.latestRequestId = request.id;
-            _configureAndInvokeXHR(request, options);
-            _requests[request.id] = request;
-            return request;
-        },
-        abort: function (all) {
-            var abortAll = all || false, requestIds;
-            var requestToAbort = abortAll ? _requests :
-            _requests[this.latestRequestId];
+    if (pz.isString(options.data)) {
+        options.data = pz.toJSON(options.data);
+    };
 
-            if (pz.isEmpty(requestToAbort)) {
-                return;
-            };
+    xhr.send(options.data || null);
+};
 
-            if (!abortAll) {
-                requestToAbort.abort();
-                delete _requests[this.latestRequestId];
-                this.latestRequestId = null;
-                return;
-            };
+return {
+    requests: {},
+    latestRequestId: null,
+    request: function (options) {
 
-            requestIds = Object.keys(requestToAbort);
-            pz.forEach(requestIds, function (id) {
-                var request = _requests[id];
-                request.abort();
-                delete _requests[id];
-                request = null;
-            }, this);
+        if (pz.isEmpty(options)) {
+            throw new Error(_const.optionsRequred);
+        };
 
-            requestToAbort = null;
+        _checkMinimalConfiguration(options);
+
+        var request = {
+            id: pz.guid(),
+            aborted: false,
+            options: options,
+            xhr: _createXHR(),
+            abort: function myfunction() {
+                this.xhr.abort();
+                this.xhr = null;
+                this.aborted = true;
+            }
+        };
+
+        this.latestRequestId = request.id;
+        _configureAndInvokeXHR(request, options);
+        _requests[request.id] = request;
+        return request;
+    },
+    abort: function (all) {
+        var abortAll = all || false, requestIds;
+        var requestToAbort = abortAll ? _requests :
+        _requests[this.latestRequestId];
+
+        if (pz.isEmpty(requestToAbort)) {
+            return;
+        };
+
+        if (!abortAll) {
+            requestToAbort.abort();
+            delete _requests[this.latestRequestId];
             this.latestRequestId = null;
-        },
-        post: function (options) {
-            options.method = _types.post;
-            return this.request(options);
-        },
-        get: function (options) {
-            options.method = _types.get;
-            return this.request(options);
-        }
-    };
-})();
+            return;
+        };
+
+        requestIds = Object.keys(requestToAbort);
+        pz.forEach(requestIds, function (id) {
+            var request = _requests[id];
+            request.abort();
+            delete _requests[id];
+            request = null;
+        }, this);
+
+        requestToAbort = null;
+        this.latestRequestId = null;
+    },
+    post: function (options) {
+        options.method = _types.post;
+        return this.request(options);
+    },
+    get: function (options) {
+        options.method = _types.get;
+        return this.request(options);
+    }
+};
