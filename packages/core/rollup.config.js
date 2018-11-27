61d2ea0134193ea125f1c1530480cd6bb3a2230e
Milos <protic023@gmail.com>
2018-11-27 20:57:28 +0100

    added rollup banner

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,13 +1,14 @@
 import commonRollup from '../../scripts/rollup-common.config';
 import path from 'path';
+import pck from './package.json';
 
-const pck = require('./package.json');
 const src = path.resolve(__dirname, 'src')
 const dist = path.resolve(__dirname, 'dist');
-const config = Object.assign({}, commonRollup);
+const config = Object.assign({}, commonRollup.config);
 
 config.input = path.resolve(src, 'index.js');
 config.output.name = pck.rollupName;
-config.output.file = path.resolve(dist, (pck.rollupFileName + '.js'))
+config.output.banner = commonRollup.buildBanner(pck);
+config.output.file = path.resolve(dist, (pck.rollupFileName + '.js'));
 
 export default config;
--- scripts/rollup-common.config.js
+++ scripts/rollup-common.config.js
@@ -3,18 +3,27 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
 export default {
-    input: '',
-    output: {
-        name: '',
-        file: '',
-        format: 'umd'
+    config: {
+        input: '',
+        output: {
+            name: '',
+            file: '',
+            format: 'umd'
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            babel({
+                exclude: ['node_modules/**'],
+                presets: ['@babel/preset-env']
+            })
+        ]
     },
-    plugins: [
-        resolve(),
-        commonjs(),
-        babel({
-            exclude: ['node_modules/**'],
-            presets: ['@babel/preset-env']
-        })
-    ]
+    buildBanner: (pck) => {
+        return [
+            '// PlazarJS ' + pck.rollupFileName + ' v' + pck.version,
+            '// Copyright ' + pck.author + ' and other contributors',
+            '// Licensed under ' + pck.license + ' (https://github.com/ProticM/plazar-js/blob/master/LICENSE)'
+        ].join('\n');
+    }
 };
