bf2c7b944b9339f4388a0a8cd3dd26abcd654ab4
Milos <protic023@gmail.com>
2018-11-27 09:19:52 +0100

    removed camelize and format aliases

--- packages/core/src/core/plazar-core.js
+++ packages/core/src/core/plazar-core.js
@@ -65,34 +65,6 @@ var _assignTo = function (target, source, clone) {
     return result;
 };
 
-var _camelize = function (str) {
-    if (_isEmpty(str)) {
-        return '';
-    };
-
-    return str.replace(/^([A-Z])|[\s-_](\w)/g, function (match, p1, p2, offset) {
-        if (p2) return p2.toUpperCase();
-        return p1.toLowerCase();
-    });
-};
-
-var _format = function () {
-    var args = Array.prototype.slice.call(arguments);
-    var baseString = args[0];
-    var params = args.splice(1), result = '';
-
-    if (_isEmpty(baseString) || _isEmpty(params)) {
-        return result;
-    };
-
-    _forEach(params, function (param, idx) {
-        result = _isEmpty(result) ? baseString.replace('{' + idx + '}', param) :
-            result.replace('{' + idx + '}', param);
-    });
-
-    return result;
-};
-
 var _define = function (type, object) {
 
     var cls, obj, tBase,
@@ -422,8 +394,6 @@ pz.toJSON = _toJSON;
 pz.assignTo = _assignTo;
 pz.isInstanceOf = _isInstanceOf;
 pz.getGlobal = _getGlobal;
-pz.camelize = _camelize;
-pz.format = _format;
 pz.isPzDefinition = _isPzDefinition;
 pz.deepClone = _deepClone;
 pz.isModularEnv = _isModularEnv;
--- packages/core/src/utils/string.js
+++ packages/core/src/utils/string.js
@@ -1,6 +1,13 @@
 const stringUtil = {
     camelize: (str) => {
-        return pz.camelize(str);
+        if (pz.isEmpty(str)) {
+            return '';
+        };
+    
+        return str.replace(/^([A-Z])|[\s-_](\w)/g, function (match, p1, p2, offset) {
+            if (p2) return p2.toUpperCase();
+            return p1.toLowerCase();
+        });
     },
 
     capitalize: (str) => {
@@ -17,8 +24,21 @@ const stringUtil = {
             str.indexOf(value) != -1;
     },
 
-    format: () =>  {
-        return pz.format.apply(pz, arguments);
+    function: () => {
+        var args = Array.prototype.slice.call(arguments);
+        var baseString = args[0];
+        var params = args.splice(1), result = '';
+    
+        if (_isEmpty(baseString) || _isEmpty(params)) {
+            return result;
+        };
+    
+        _forEach(params, function (param, idx) {
+            result = _isEmpty(result) ? baseString.replace('{' + idx + '}', param) :
+                result.replace('{' + idx + '}', param);
+        });
+    
+        return result;
     }
 };
 
