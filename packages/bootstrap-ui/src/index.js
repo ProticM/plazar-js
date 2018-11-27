d769c4dfbe9f566c5f66fd2267188764fcee83b5
Milos <protic023@gmail.com>
2018-11-27 13:40:01 +0100

    removed env check

--- packages/bootstrap-ui/src/index.js
+++ packages/bootstrap-ui/src/index.js
@@ -4,10 +4,10 @@ import * as components from './components/index';
 const bootstrapUi = {
     init: (pz) => {
         let b = pz.component.extend(base);
-        if(!pz.isModularEnv()) { pz.storeDefinition(b.$type, b) };
+        pz.storeDefinition(b.$type, b);
         pz.forEach(components.all, component => {
             let def = b.extend(component);
-            if(!pz.isModularEnv()) { pz.storeDefinition(def.$type, def) };
+            pz.storeDefinition(def.$type, def);
         });
     }
 };
