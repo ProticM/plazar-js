e28e65a2ce7caa87b33d7f83724f7569a3e5c655
Milos <protic023@gmail.com>
2018-11-27 16:36:46 +0100

    updated bootstrap components export

--- packages/bootstrap-ui/src/index.js
+++ packages/bootstrap-ui/src/index.js
@@ -1,15 +1,15 @@
 import base from './base/ui-base.component';
-import * as components from './components/index';
+import * as components from './components';
 import pz from '@plazarjs/core';
 
 const bootstrapUi = {
     init: () => {
         let b = pz.component.extend(base);
         pz.storeDefinition(b.$type, b);
-        pz.forEach(components.components, component => {
-            let def = b.extend(component);
+        for(let component in components) {
+            let def = b.extend(components[component]);
             pz.storeDefinition(def.$type, def);
-        });
+        };
     }
 };
 
