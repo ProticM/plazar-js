fecd3eaf624eea3da0fad5ed07b72e5afe723476
Milos <protic023@gmail.com>
2018-12-07 15:48:50 +0100

    updated core package tests

--- packages/core/__tests__/core.test.js
+++ packages/core/__tests__/core.test.js
@@ -3,7 +3,32 @@
 import pz from '../src/core';
 
 describe('core', function() {
-    it('should be defined', () => {
-        expect(pz).toBeDefined();
+    let obj = {}, fn = () => {}, array = [], und = undefined, 
+        n = null, emptyStr = '', str = 'test';
+
+    it('should be object', () => {
+        expect(pz.isObject(obj)).toBe(true);
+    });
+
+    it('should be function', () => {
+        expect(pz.isFunction(fn)).toBe(true);
+    });
+
+    it('should be array', () => {
+        expect(pz.isArray(array)).toBe(true);
+    });
+
+    it('should be string', () => {
+        expect(pz.isString(str)).toBe(true);
+    });
+
+    it('should be empty', () => {
+        expect(pz.isEmpty(und)).toBe(true);
+        expect(pz.isEmpty(n)).toBe(true);
+        expect(pz.isEmpty(emptyStr)).toBe(true);
+    });
+
+    it('should not be empty', () => {
+        expect(pz.isEmpty(emptyStr, true)).toBe(false);
     });
 });
