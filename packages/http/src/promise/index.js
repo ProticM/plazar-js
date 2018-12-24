76fcc52a49beb5b4c098e158ad4420c191fce10f
Milos <protic023@gmail.com>
2018-12-24 09:19:31 +0100

    removed states from the instance, functions to arrow functions change

--- packages/http/src/promise/index.js
+++ packages/http/src/promise/index.js
@@ -1,14 +1,9 @@
 import handler from './handler';
-import { resolve, handle, getThen } from './util';
+import { resolve, handle, states, setResult } from './util';
 
 class promise {
     constructor(fn) {
-        this.states = {
-            PENDING: 0,
-            RESOLVED: 1,
-            REJECTED: 2
-        };
-        this.state = this.states.PENDING;
+        this.state = states.PENDING;
         this.handlers = [];
         this.value = null;
         resolve(fn, this);
@@ -50,23 +45,10 @@ class promise {
         });
     }
     resolve(value) {
-        try {
-            let then = getThen(value);
-            if(pz.isFunction(then)) {
-                return then(this.resolve, this.reject);
-            };
-
-            this.state = this.states.RESOLVED;
-            this.value = value;
-            return handle(this);
-        } catch(e) {
-            return this.reject(e);
-        };
+        return setResult(this, value, states.RESOLVED);
     }
     reject(value) {
-        this.state = this.states.REJECTED;
-        this.value = value;
-        return handle(this);
+        return setResult(this, value, states.REJECTED);
     }
 }
 
--- packages/http/src/promise/util.js
+++ packages/http/src/promise/util.js
@@ -1,6 +1,12 @@
 import pz from '@plazarjs/core';
 
-function resolve(fn, promise) {
+const states = {
+    PENDING: 0,
+    RESOLVED: 1,
+    REJECTED: 2
+};
+
+let resolve = (fn, promise) => {
     try {
         fn(promise.resolve.bind(promise), promise.reject.bind(promise));
     } catch(e) {
@@ -8,18 +14,18 @@ function resolve(fn, promise) {
     };
 };
 
-function getThen(value) {
+let getThen = (value) => {
     return !pz.isEmpty(value) && (pz.isObject(value) || pz.isFunction(value)) 
         && pz.isFunction(value.then) ? value.then : null;
 };
 
-function handle(promise) {
-    if (promise.state === promise.states.PENDING) {
+let handle = (promise) => {
+    if (promise.state == states.PENDING) {
         return null;
     };
   
     promise.handlers.forEach((handler) => {
-        if (promise.state === promise.states.REJECTED) {
+        if (promise.state == states.REJECTED) {
             return handler.onFail(promise.value);
         };
 
@@ -29,8 +35,30 @@ function handle(promise) {
     pz.arr.clear(promise.handlers);
 };
 
+let setResult = (promise, value, state) => {
+    setTimeout(() => {
+        try {
+            if (promise.state != states.PENDING) {
+                return null;
+            };
+    
+            let then = getThen(value);
+            if(pz.isFunction(then)) {
+                return then(promise.resolve, promise.reject);
+            };
+    
+            promise.state = state;
+            promise.value = value;
+            return handle(promise);
+        } catch(e) {
+            return promise.reject(e);
+        };
+    }, 0);
+};
+
 export {
     resolve,
-    getThen,
-    handle
+    handle,
+    setResult,
+    states
 }
